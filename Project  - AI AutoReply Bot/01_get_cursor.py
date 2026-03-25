c75a095f8991d77c7b21f371ccaa2c5dcff96efa
Syed_Najam <syednajam.b001@gmail.com>
2026-03-25 18:26:47 +0500

    Add files via upload

---
+++ Project  - AI AutoReply Bot/01_get_cursor.py
@@ -0,0 +1,15 @@
+import pyautogui
+
+while True:
+    a = pyautogui.position()
+    print(a)
+
+ 
+ # my laptop coordinates 
+ # note the coordinates according to your laptop 
+ # 1172 142
+ # 1414 902 
+
+ # 1459 1040   
+ # 1544 859
+ # 1294 941
